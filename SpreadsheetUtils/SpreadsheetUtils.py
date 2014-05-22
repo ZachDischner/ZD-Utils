334fbff047e6df092d3dfb0d57f4377dc5c117ff
Zach Dischner <zach.dischner@colorado.edu>
2014-05-21 18:48:18 -0600

    Updated spreadsheet utils, better docstrings

--- SpreadsheetUtils/SpreadsheetUtils.py
+++ SpreadsheetUtils/SpreadsheetUtils.py
@@ -1,19 +1,28 @@
-__author__ = 'zachdischner'
-#
-# File name: SpreadsheetUtils.py
-# Authors: Zach Dischner
-# Created: 5/14/2014
-#
-# Description:
-#   Utilities for dealing with spreadsheets. Yay.
-#
-# Function List:
-#
-#
-# Todos:
-#
-#
+##! /usr/bin/python
+__author__ = 'Zach Dischner'
+__copyright__ = "NA"
+__credits__ = ["NA"]
+__license__ = "NA"
+__version__ = "1.0.0"
+__maintainer__ = "Zach Dischner"
+__email__ = "zach.dischner@gmail.com"
+__status__ = "Dev"
 
+"""
+File name: SpreadsheetUtils.py
+Authors: Zach Dischner
+Created: 1/1/2014
+Modified: 5/21/2015
+
+@brief This module provides some basic functions for parsing spreadsheets/csv/tab deliminated files
+
+The aim is to provide common utilities to format said file into a Pandas dataframe that is easily manipulatable by the
+user. 
+
+Have fun! 
+
+@todos None for now!  Might want to make this into a class that wraps a dataframe. IDK.
+"""
 
 # -------------------------
 # --- IMPORT AND GLOBAL ---
@@ -23,32 +32,19 @@ import sys as sys
 import numpy as np
 
 # ------------
-# --- import_CMD_sheet ---
-# ------------
-def import_CMD_sheet(fname, sheetname='PDIR_CMD', no_spaces=True):
-    """This function parses a CMD spreadsheet formatted as Excel spreadsheet.
-    Run: 'help(import_CMD_sheet)' in interactive mode to print this statement out
-    Input:  fname - string with filename and path of excel spreadsheet to open
-    Returns: data - Pandas data frame
-
-    Example: data = import_excel("CMD_FILES/cmd_Jan_01_2001.xls")
-    """
-    data = pd.read_excel(fname, sheetname, header=1)
-    if no_spaces is True:
-        data = reformat_column_names(data)
-    return data
-
-
-# ------------
 # --- import_excel ---
 # ------------
 def import_excel(fname, sheetname='PDIR_CMD',no_spaces=True):
-    """This function parses a CMD spreadsheet formatted as Excel spreadsheet.
-    Run: 'help(import_excel)' in interactive mode to print this statement out
-    Input:  fname - string with filename and path of excel spreadsheet to open
-    Returns: data - Pandas data frame
+    """@brief This function imports an excel spreadsheet, converts to a pandas dataframe.
 
-    Example: data = import_excel("CMD_FILES/cmd_Jan_01_2001.xls")
+    @param [fname]  String with filename and path of excel spreadsheet to open
+    @param [sheetname='Sheet1'] Name of sheet to parse. 
+    @param [no_spaces=True] Boolean indicator to remove spaces and fill with '_'
+    
+    @retval [data]  Pandas data frame
+
+     \bExample
+         \code data = import_excel("CMD_FILES/cmd_Jan_01_2001.xls")\endcode
     """
     data = pd.read_excel(fname, sheetname, header=1)
     if no_spaces is True:
@@ -63,12 +59,15 @@ def import_excel(fname, sheetname='PDIR_CMD',no_spaces=True):
 # --- import_csv ---
 # ------------
 def import_csv(fname, no_spaces=True):
-    """This function parses a CMD spreadsheet formatted as csv.
-    Run: 'help(parst_csv)' in interactive mode to print this statement out
-    Input: fname - string with filename and path of csv to open
-    Returns: data - Pandas data frame
+    """@brief This function parses a CMD spreadsheet formatted as Excel spreadsheet.
 
-    Example: data = import_csv("CMD_FILES/cmd_Jan_01_2001.csv")
+    @param [fname]  String with filename and path of excel spreadsheet to open
+    @param [no_spaces=True] Boolean indicator to remove spaces and fill with '_'
+    
+    @retval [data]  Pandas data frame
+
+     \bExample
+         \code data = import_csv("CMD_FILES/cmd_Jan_01_2001.csv")\endcode
     """
     data = pd.read_csv(fname, header=1)
     # Reformat column indices to replace spaces with underscores
@@ -83,34 +82,44 @@ def import_csv(fname, no_spaces=True):
 # ------------
 # --- mask ---
 # ------------
-# *Objectify this?, get rid of the "dataframe" argument
 def mask(dataframe, key, value):
-    """This function masks a dataframe, given a key-value pair
-    Run: 'help(mask)' in interactive mode to print this statement out
-    Input:  dataframe - a pandas dataframe containing all or part of a CMD spreadsheet
-            key - "key" accessing dataframe columns
-            value - the value that you wish to mask by dataframe[key]
-    Returns: dataframe - Pandas data frame
+    """@brief This function masks a dataframe, given a key-value pair
 
-    Example: data = mask(full_dataframe,"CMD_NAME","On")
+    Key/Value pair can be a string, or number. Logic handles both 
+    NOTE that this works based on string.contains, not exact comparison. Take this into account   
+
+    @param [dataframe: Pandas dataframe]  a pandas dataframe 
+    @param [key: string] "key" accessing dataframe columns
+    @param [value: string or numeric] the value you wish to mask by dataframe[key]
+    
+    @retval [data]  Masked pandas data frame
+
+     \bExample
+         \code data = mask(full_dataframe,"CMD_NAME","On") \endcode
     """
+    
+    if type(key) == str:
+        return df.loc[(df[key]).apply(string.upper).str.contains()]
+
     return dataframe[dataframe[key] == value]
 
 # ------------
 # --- drop_columns ---
 # ------------
-def drop_columns(dataframe, matching="Unnamed"):
-    """Removes colums with names matching the 'matching' input
-    Run: 'help(drop_columns)' in interactive mode to print this statement out
-    Input:  matching (op) - string indicating with dataframe colums are to be dropped
-    Returns: dataframe - Pandas data frame
+def drop_columns(dataframe, drop_key="Unnamed"):
+    """@brief Removes colums with names specified by input
 
-    Example: data = drop_columns("Unnamed")
-            # Removes all columns that look like "Unnamed*"
+    @param [dataframe: Pandas dataframe]  a pandas dataframe 
+    @param [drop_key='Unnamed'] string indicating which dataframe columns to drop
+    
+    @retval [dataframe]  New dataframe with columns dropped
+
+     \bExample
+         \coded ata = drop_columns("Unnamed") \endcode
     """
     dcols = dataframe.columns
     for col in dcols:
-        if col.find("Unnamed") >= 0:
+        if col.find(drop_key) >= 0:
             dataframe = dataframe.drop(col, axis=1)
     return dataframe
 
@@ -119,16 +128,17 @@ def drop_columns(dataframe, matching="Unnamed"):
 # --- reformat_column_names ---
 # ------------
 def reformat_column_names(dataframe, replace_this=" ", with_this="_"):
-    """This function is a simple utility to replace string patterns in colum names with something more helpful.
+    """@brief This function is a simple utility to replace string patterns in colum names with something more helpful.
     By default, it replaces spaces in column names with underscores (just to make life easier)
-    Run: 'help(reformat_column_names)' in interactive mode to print this statement out
-    Input:      *dataframe      - dataframe to manipulate
-                *(replace_this) - string to search for
-                *(with_this)    - replacement string when matches are found
 
-    Returns:    *dataframe      - Pandas dataframe with adjusted column names
+    @param [dataframe: Pandas dataframe]  a pandas dataframe 
+    @param [replace_this=" "] A string to be replaced in column names
+    @param [with_this="_"] Replacement string when matches are found
+    
+    @retval [dataframe]  New dataframe with renamed columns
 
-    Example:    dataframe = Parse.reformat_column_names(dataframe,replace_this="data.",with_this="data_")
+     \bExample
+         \code dataframe = Parse.reformat_column_names(dataframe,replace_this="data.",with_this="data_") \endcode
     """
     dataframe.columns = [col.replace(replace_this, with_this) for col in dataframe.columns]
     return dataframe
---
+++ SpreadsheetUtils/__init__.py
@@ -0,0 +1,8 @@
+__author__ = 'Zach Dischner'
+__copyright__ = "NA"
+__credits__ = ["NA"]
+__license__ = "NA"
+__version__ = "1.0.0"
+__maintainer__ = "Zach Dischner"
+__email__ = "zach.dischner@gmail.com"
+__status__ = "Dev"
